1cff63fde28e908db4f39b722d947a08e20cd63c
xiangxia <xia.xiang@mindertech.co.uk>
2021-04-30 14:45:22 +0800

    no message

---
+++ components/category-item/category-item.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="list-item">
+		<!-- <image :src="item.photos | formatThumb"></image> -->
+		<image class="icon" src="../../static/test/2.jpeg" mode="aspectFill"></image>
+		<view class="content">
+			<view>
+				<text>{{item.localeName}}</text>
+			</view>
+			<view>报盘号{{item.number}}</view>
+			<view>货源地</view>
+			<view>预计装柜重量</view>
+			<view>预计柜数</view>
+			<view>价格</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"category-item",
+		data() {
+			return {
+				
+			};
+		},
+		props: {
+			item: {
+				type: Object
+			}
+		}
+	}
+</script>
+
+<style>
+	.list-item {
+		background-color: #8F8F94;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		margin-top: 15px;
+		margin-left: $xx-spaceing-hmargin;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		margin-left: 5px;
+	}
+	.icon {
+		width: 130px;
+		height: 130px;
+		margin-left: 15px;
+		border-radius: 10px;
+	}
+</style>
--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,21 +1,10 @@
 <template>
-	<view>
+	<view class="parent">
 		<category-bar class="top"></category-bar>
-		<view class="list-item" v-for="(item, index) in listings" :key="index">
-			<!-- <image :src="item.photos | formatThumb"></image> -->
-			<image class="icon" src="../../static/test/2.jpeg" mode="aspectFill"></image>
-			<view class="content">
-				<view>
-					<text>{{item.localeName}}</text>
-				</view>
-				<view>报盘号{{item.number}}</view>
-				<view>货源地</view>
-				<view>预计装柜重量</view>
-				<view>预计柜数</view>
-				<view>价格</view>
-			</view>
-		</view>
-		<load-more-item :hasMoreData="hasMoreData"></load-more-item>
+		<scroll-view class="bottom" >
+			<category-item v-for="(item, index) in listings" :key="index" :item="item"></category-item>
+			<load-more-item :hasMoreData="hasMoreData"></load-more-item>
+		</scroll-view>
 	</view>
 </template>
 
@@ -68,23 +57,13 @@
 </script>
 
 <style>
-	.list-item {
-		background-color: #8F8F94;
-		width: 100%;
-		display: flex;
-		flex-direction: row;
-		margin-top: 15px;
-		margin-left: $xx-spaceing-hmargin;
+	.parent {
+		
 	}
-	.content {
-		display: flex;
-		flex-direction: column;
-		margin-left: 5px;
+	.top {
+		
 	}
-	.icon {
-		width: 130px;
-		height: 130px;
-		margin-left: 15px;
-		border-radius: 10px;
+	.bottom {
+		
 	}
 </style>
